5bde51cc9577e1d012dd4ec2fd87d443ea8f16e5
Rahulappus <rahulappums@gmail.com>
2022-07-08 12:54:30 +0530

    gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewGalleryComponent } from './new-gallery/new-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewGalleryComponent
   ],
   imports: [
     BrowserModule,
